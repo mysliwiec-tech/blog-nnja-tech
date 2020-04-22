262e38b2f6152cf10671aaed3b4f0ffec765a529
Dariusz Myśliwiec <mysliwiec@protonmail.com>
2020-04-23 00:15:45 +0200

    enabling code view

--- components/sections/code.js
+++ components/sections/code.js
@@ -1,10 +1,12 @@
+import IframeResizer from 'iframe-resizer-react'
+
 const Code = (props) => (
     <div>
-        <iframe
+        <IframeResizer
             src={`https://carbon.now.sh/embed/${props.gist}?bg=rgba(255%2C255%2C255%2C0)&dsyoff=10px&dsblur=31px`}
-            style={{ width: '650px', height: props.height, border: 0, overflow: 'hidden'}}
+            style={{ width: '100%', minHeight: props.height + 'px', border: 0}}
             sandbox="allow-scripts allow-same-origin">
-        </iframe>
+        </IframeResizer>
     </div>
 )
 
