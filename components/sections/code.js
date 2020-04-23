991ed13bd34cc43a8eb5c9d19f881378722170cf
Dariusz Myśliwiec <mysliwiec@protonmail.com>
2020-04-23 13:32:57 +0200

    proper iframe attributes

--- components/sections/code.js
+++ components/sections/code.js
@@ -1,13 +1,17 @@
 import IframeResizer from 'iframe-resizer-react'
+import { RichText } from 'prismic-reactjs'
 
-const Code = (props) => (
+const Code = (props) => {
+    let lang = props.data.lang;
+    const code = RichText.asText(props.data.code);
+    return (
     <div>
         <IframeResizer
-            src={`https://carbon.now.sh/embed/${props.gist}?bg=rgba(255%2C255%2C255%2C0)&dsyoff=10px&dsblur=31px`}
-            style={{ width: '100%', minHeight: props.height + 'px', border: 0}}
+            src={`https://nnja-carbon.now.sh/embed?code=${code}&l=${lang}&bg=rgba(255%2C255%2C255%2C0)&dsyoff=10px&dsblur=31px`}
+            style={{ width: '1px', minWidth: '100%', border: 0}}
             sandbox="allow-scripts allow-same-origin">
         </IframeResizer>
     </div>
-)
+)}
 
 export default Code
