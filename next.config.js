e2d7f9d851c0f47dea1fc89e939a4bde6581f415
Dariusz Myśliwiec <mysliwiec@protonmail.com>
2020-05-07 00:12:24 +0200

    different next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const path = require('path');
 
-module.exports = 
-withPlugins([
-  {
-    webpack: function(config) {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      })
-      return config
-    },
+const nextConfig = {
+  webpack: function(config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      include: [
+        path.resolve(__dirname, 'posts')
+      ],
+      use: 'raw-loader',
+    })
+    return config
   },
-  [optimizedImages, {
-    optimizeImagesInDev: true,
-  }],
-])
+};
+
+const optimizedImagesConfig = {
+  optimizeImagesInDev: false,
+}
+
+module.exports = withPlugins([
+  [optimizedImages, optimizedImagesConfig],
+], nextConfig);
